38f82bee144976ba317282f95a5b2be71731038f
unknown <1784350868@qq.com>
2020-09-13 18:27:43 +0800

    data processing

--- test/gen_dataset_chaos.py
+++ test/gen_dataset_chaos.py
@@ -1,9 +1,10 @@
 import os
-import SimpleITK as sitk
-from skimage.io import imread, imsave
-import numpy as np
 import random
 import shutil
+import pydicom
+from skimage import img_as_float
+import matplotlib.pyplot as plt
+import cv2 as cv
 PATH = "../../DataSet"
 MODEL = "Chaos_Challenge"
 SOURCE = "MR"
@@ -50,17 +51,53 @@ def gen_data():
         patient_path = os.path.join(DATA_ROOT,patient_id)
         gt,img = get_T1_filename(patient_path)
         for j in range(len(gt)):
-            output = os.path.join(PATH,MODEL,model,"GT","Subj_"+str(i)+"slice_"+str(j+1)+".png")
+            output = os.path.join(PATH,MODEL,model,"GT_Whole","Subj_"+patient_id+"slice_"+str(j+1)+".png")
             shutil.copy(gt[j],output)
 
         for j in range(len(img)):
-            output = os.path.join(PATH,MODEL,model,"Img","Subj_"+str(i)+"slice_"+str(j+1)+".dcm")
+            output = os.path.join(PATH,MODEL,model,"DCM","Subj_"+patient_id+"slice_"+str(j+1)+".dcm")
             shutil.copy(img[j],output)
 
 def dcm_to_png(model = "train"):
     assert model in ["train","val","test"]
-    data_dirs = os.path.join(PATH,MODEL,model)
-    data_name = os.listdir(data_dirs)
+    data_dirs = os.path.join(PATH,MODEL,model,"DCM")
+    data_names = os.listdir(data_dirs)
+    for i in range(len(data_names)):
+        dcm_name = os.path.join(PATH,MODEL,model,"DCM",data_names[i])
+        save_name = os.path.join(PATH,MODEL,model,"Img",data_names[i].split(".")[0]+".png")
+        dcm = pydicom.dcmread(dcm_name)
+        img_array = dcm.pixel_array
+        temp = img_array.copy()
+        max = img_array.max()
+        vmin = img_array.min()
+        vmax = temp[temp<max].max()
+        img_array[img_array > vmax] = 0
+        img_array[img_array < vmin] = 0
+        img_array = img_as_float(img_array)
+        plt.cla()
+        plt.figure('adjust_gamma', figsize=(256/100, 256/100))
+        plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0, wspace=0)
+        plt.imshow(img_array, 'gray')
+        plt.axis('off')
+        plt.savefig(save_name)
 
+def gt_to_liver(model = "train"):
+    assert model in ["train","val","test"]
+    data_dirs = os.path.join(PATH,MODEL,model,"GT_Whole")
+    data_names = os.listdir(data_dirs)
+    for i in range(len(data_names)):
+        gt_name = os.path.join(PATH,MODEL,model,"GT_Whole",data_names[i])
+        save_name = os.path.join(PATH,MODEL,model,"GT",data_names[i])
+        src = cv.imread(gt_name)
+        for i in range(src.shape[0]):
+            for j in range(src.shape[1]):
+                for k in range(src.shape[2]):
+                    if 55 <= src.item(i, j, k) <= 70:
+                        src.itemset((i, j, k), 255)
+                    else:
+                        src.itemset((i, j, k), 0)
+        cv.imwrite(save_name, src)
 
-gen_data()
+gt_to_liver(model="train")
+gt_to_liver(model="test")
+gt_to_liver(model="val")
--- test/test_grammar.py
+++ test/test_grammar.py
@@ -1,3 +1,9 @@
 import torch
-a = torch.zeros(64,64,255,255)
-print(a.size()[2:])
+import os
+import cv2 as cv
+root = "../../DataSet/Chaos_Challenge/train/GT"
+img_names = os.listdir(root)
+for name in img_names:
+    img = cv.imread(os.path.join(root,name))
+    if  not img.shape[0] == 256 :
+        print(name)
